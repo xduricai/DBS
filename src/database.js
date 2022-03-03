02673dcd9c5f1c033bc6b29a62eede01401aedda
Igor Ďurica <79131734+xduricai@users.noreply.github.com>
2022-03-03 14:35:41 +0100

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,4 +1,9 @@
 const { Client } = require("pg")
+let types = require('pg').types
+
+types.setTypeParser(20, function(val) {
+    return parseInt(val, 10)
+})
 
 const client = new Client()
 client.connect()
